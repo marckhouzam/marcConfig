8c9714804fb82e4ea66f0251a06c947fa6e262ba
Marc Khouzam <marc.khouzam@gmail.com>
2018-01-09 09:35:47 -0500

    Add K8s prompt

---
+++ zsh/k8s_prompt.zsh
@@ -0,0 +1,17 @@
+autoload -U colors; colors
+
+prompt_kubecontext() {
+  kubectx="<- $(kubectl config current-context) ->"
+  if [[ ${kubectx} == *"local"* ||
+        ${kubectx} == *"minikube"* ||
+        ${kubectx} == *"lab"* ]]; then
+        prompt_segment black green "${kubectx}"
+  elif [[ ${kubectx} == *"dev"* ||
+          ${kubectx} == *"acc"* ]]; then
+        prompt_segment black yellow "${kubectx}"
+  elif [[ ${kubectx} == *"prod"* ||
+          ${kubectx} == *"infra"* ]]; then
+        prompt_segment black red "${kubectx}"
+  fi
+}
+RPROMPT='$(prompt_kubecontext)'
